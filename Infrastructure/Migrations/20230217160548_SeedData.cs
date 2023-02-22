70f3f669e95a737fad5a0c8f91201cc0f5f04974
Bereket Yimer <bekym0242@gmail.com>
2023-02-22 11:13:12 +0300

    seed data menu items update

--- Infrastructure/Migrations/20230217160548_SeedData.cs
+++ Infrastructure/Migrations/20230217160548_SeedData.cs
@@ -12,10 +12,19 @@ namespace Infrastructure.Migrations
         {
             migrationBuilder.Sql(@"
 				INSERT INTO public.""Category""(""Name"", ""CreateDateTime"", ""ModifiedDateTime"")
-					VALUES ('ቁርስ', CURRENT_DATE, null);
+					VALUES ('ቁርስ', CURRENT_DATE, null),
+                           ('ምሳ', CURRENT_DATE, null),
+                           ('መጠጥ', CURRENT_DATE, null);
 
 				INSERT INTO public.""Group""(""CatagoryId"", ""Name"", ""CreateDateTime"", ""ModifiedDateTime"")
-					VALUES (1, 'ቁርስ', CURRENT_DATE, null);
+					VALUES (1, 'ቁርስ', CURRENT_DATE, null),
+						   (2, 'ራፕ', CURRENT_DATE, null),
+						   (2, 'ሳንድዊች', CURRENT_DATE, null),
+						   (2, 'የሀበሻ ምግቦች', CURRENT_DATE, null),
+						   (3, 'እርጎ', CURRENT_DATE, null),
+						   (3, 'ትኩስ መጠጥ', CURRENT_DATE, null),
+						   (3, 'አይስድ መጠጥ', CURRENT_DATE, null),
+						   (3, 'ውሃ እና ለስላሳ', CURRENT_DATE, null);
 
 				INSERT INTO public.""MenuItem""(""Name"", ""UnitPrice"", ""GroupId"", ""CreateDateTime"", ""ModifiedDateTime"")
 					VALUES ('ኮንቲነንታል', 164, 1, CURRENT_DATE, null),
@@ -31,7 +40,50 @@ namespace Infrastructure.Migrations
 							('እረፍት ፈጢራ', 130, 1, CURRENT_DATE, null),
 							('ቺፕስ', 55, 1, CURRENT_DATE, null),
 							('ጭማሪ ዳቦ', 10, 1, CURRENT_DATE, null),
-							('ጭማሪ ቺዝ', 10, 1, CURRENT_DATE, null)		
+							('ጭማሪ ቺዝ', 10, 1, CURRENT_DATE, null),
+							('ቺክን ራፕ',164 , 2, CURRENT_DATE, null),
+							('ቱና ራፕ',150 , 2, CURRENT_DATE, null),
+							('አቮካዶ ቺክን ራፕ',166 , 2, CURRENT_DATE, null),
+							('ክለብ ሳንድዊች',210 , 3, CURRENT_DATE, null),
+							('ግማሽ  ክለብ ሳንድዊች',120 , 3, CURRENT_DATE, null),
+							('ስፔሻል ቺክን ሳንድዊች',200 , 3, CURRENT_DATE, null),
+							('ቱና ሳንድዊች',148 , 3, CURRENT_DATE, null),
+							('ሳንድዊች',149 , 3, CURRENT_DATE, null),
+							('እንቁላል ሳንድዊች',137 , 3, CURRENT_DATE, null),
+							('ተጋቢኖ',110 , 4, CURRENT_DATE, null),
+							('ቦዘና',156 , 4, CURRENT_DATE, null),
+							('ቋንጣ ፍርፍር',161 , 4, CURRENT_DATE, null),
+							('ጥብስ ፍርፍር',164 , 4, CURRENT_DATE, null),
+							('ፍርፍር በቅቤ',110 , 4, CURRENT_DATE, null),
+							('የጾም ፍርፍር',100 , 4, CURRENT_DATE, null),
+							('ዱለት',116 , 4, CURRENT_DATE, null),
+							('ጥብስ',228 , 4, CURRENT_DATE, null),
+							('ሽሮ በቅቤ',120 , 4, CURRENT_DATE, null),
+							('ኖርማል እርጎ',50, 5, CURRENT_DATE, null),
+							('ፍሌቨርድ እርጎ',60, 5, CURRENT_DATE, null),
+							('ስፔሻል እርጎ',60, 5, CURRENT_DATE, null),
+							('ኤስፕረሶ',25, 6, CURRENT_DATE, null),
+							('አሜሪካኖ',25, 6, CURRENT_DATE, null),
+							('ስፕሪስ ሻይ',25, 6, CURRENT_DATE, null),
+							('ማኪያቶ',25, 6, CURRENT_DATE, null),
+							('የጀበና ቡና',15, 6, CURRENT_DATE, null),
+							('ሻይ',20, 6, CURRENT_DATE, null),
+							('ዝንጅብል ሻይ',25, 6, CURRENT_DATE, null),
+							('ሃቢስከስ ሻይ',25, 6, CURRENT_DATE, null),
+							('ስፔሻል ሻይ',30, 6, CURRENT_DATE, null),
+							('ወተት',30, 6, CURRENT_DATE, null),
+							('ካፑቺኖ',40, 6, CURRENT_DATE, null),
+							('ብርቱካን ሻይ',30, 6, CURRENT_DATE, null),
+							('ለውዝ ሻይ',30, 6, CURRENT_DATE, null),
+							('የማሽን ቡና',25, 6, CURRENT_DATE, null),
+							('አይስድ ቡና',50, 7, CURRENT_DATE, null),
+							('አይስድ ሻይ',40, 7, CURRENT_DATE, null),
+							('አይስድ ላቴ',50, 7, CURRENT_DATE, null),
+							('ለስላሳ(1/2Lt)',30, 8, CURRENT_DATE, null),
+							('ውሃ(1/2Lt)',15, 8, CURRENT_DATE, null),
+							('ውሃ(1Lt)',20, 8, CURRENT_DATE, null);
+							
+ 
             ");
         }
 
